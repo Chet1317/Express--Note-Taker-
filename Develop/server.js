7304a52672b72ffff4f241104ae0f8330dc22c90
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-29 19:53:07 -0400

    inserted savenotes function into post and delete apps

--- Develop/server.js
+++ Develop/server.js
@@ -11,9 +11,7 @@ app.use (express.urlencoded({extended:true}))
 
 const fileName = db.json
 
-let noteList = [
-    { title: "first note placeholder title", text: "first note text", id: 1000}
-]
+let noteList = loadNotes
 
 function saveNotes(){
     fs.writeFileSync(fileName, JSON.stringify(noteList))
@@ -33,7 +31,7 @@ app.post("/api/notes", function(req, res){
     let newNote = req.body
     newNote.id = Date.now()
 noteList.push(newNote)
-
+saveNotes()
 res.send(newNote)
     
 });
@@ -41,7 +39,7 @@ res.send(newNote)
 app.delete("/api/notes/:id", function(req, res){
     const noteId = req.params.id
     res.send({id: noteId})
-
+    saveNotes()
     noteList = noteList.filter(note =>note.id != noteId)
 });
 
