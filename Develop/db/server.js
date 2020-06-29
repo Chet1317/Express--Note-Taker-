fe23bc924125581bab140bd5bb50347d0d4831ee
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-06-29 12:20:01 -0400

    updated listener function

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -10,6 +10,6 @@ app.use (express.urlencoded({exntended:true}))
 
 
 
-app.listen("/api/notes", function(){
+app.listen("/api/notes:id", function(){
     console.log('App listening on PORT' + PORT)
 });
