019af409e1c77a7c75c28f7eac3e97418a7ccb08
Achilles <63617922+Chet1317@users.noreply.github.com>
2020-07-03 14:55:25 -0400

    made improvments to files

--- Develop/server.js
+++ Develop/server.js
@@ -1,49 +1,49 @@
 const express = require("express");
 const fs = require("fs");
 const app = express ();
-const PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
 app.use(express.json())
 app.use (express.urlencoded({extended:true}))
 
-const fileName = "db/db.json"
+const file = "db/db.json"
 
-function saveNotes(){
-    fs.writeFileSync(fileName, JSON.stringify(noteList))
+function savedNotes(){
+    fs.writeFileSync(file, JSON.stringify(noteList))
 }
 
-function loadNotes(){
-    const loadNotes= JSON.parse(fs.readFileSync(fileName, "utf8"))
+function loadingNotes(){
+    const loadNotes= JSON.parse(fs.readFileSync(file, "utf8"))
     return loadNotes
 }
 
-let noteList = loadNotes()
+let list = loadingNotes()
 
 app.get("/api/notes", function(req, res){
-   res.send(noteList)
-
+   res.send(list)
+   
 });
 
 app.post("/api/notes", function(req, res){
     let newNote = req.body
     newNote.id = Date.now()
-noteList.push(newNote)
-saveNotes()
+list.push(newNote)
+savedNotes()
 res.send(newNote)
     
 });
 
 app.delete("/api/notes/:id", function(req, res){
-    const noteId = req.params.id
-    res.send({id: noteId})
-    saveNotes()
-    noteList = noteList.filter(note =>note.id != noteId)
+    const notesId = req.params.id
+    res.send({id: notesId})
+    savedNotes()
+    list = list.filter(note =>note.id != noteId)
 });
 
 
 
 app.listen(PORT, function(){
-    console.log('App listening on PORT' + PORT)
+    console.log(`App listening on PORT ${PORT}`)
 });
